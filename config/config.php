7069964cc55db5188f9c4d06744768ff7fae4ec6
wolfgangserque <wolfgangserque@gmail.com>
2024-06-18 14:28:34 +0700

    first

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+$koneksi=mysqli_connect('localhost','root','','db_gudang2');
+// if (!$koneksi){
+//     die("Koneksi Gagal". mysqli_connect_error());
+// }else {
+//     echo "Koneksi Berhasil Bre";
+// }
+
+?>
