4667583a10d4051397e63d4018690f56f9298ef4
wolfgangserque <wolfgangserque@gmail.com>
2024-07-08 00:37:11 +0700

    copyright

--- app/index.php
+++ app/index.php
@@ -113,7 +113,7 @@ if(!$_SESSION['nama']){
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; Gudangku</span>
                     </div>
                 </div>
             </footer>
